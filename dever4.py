693d83d6db4fcf8686bc9b5cfe8144bc61f4eb4d
Henrique Vieira <henriquevieiranois@gmail.com>
2025-03-14 13:33:48 -0300

    Dever 4

---
+++ dever4.py
@@ -0,0 +1,11 @@
+def F(n):
+    if n == 1:
+        return 2
+    return 2 * F(n - 1) + n**2
+
+# Solicita ao usuário um valor de n
+n = int(input("Digite um valor para n: "))
+
+# Calcula e exibe o resultado
+resultado = F(n)
+print(f"F({n}) = {resultado}")
